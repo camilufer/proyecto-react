b700d4d2dfce4dd04001912e9702231565918b3e
camilufer <camilu.fer21@gmail.com>
2018-03-14 17:12:20 -0300

    agregando axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      people: [],
+    }  
+  }
+
+  getPeople(){
+    return axios.get("https://swapi.co/api/people")
+    .then((response) => {
+      console.log(response.data.results);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
       </div>
     );
   }
