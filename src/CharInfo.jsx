ca6e46d67f86cfe212d727c140f0d0f01ee499c6
Lore Palominos <lorena.palominos@gmail.com>
2018-03-14 23:40:12 -0300

    avance

--- src/CharInfo.jsx
+++ src/CharInfo.jsx
@@ -1,7 +1,25 @@
 import React, {Component} from 'react';
 
 class CharInfo extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      expanded: false,
+    }
+  }
+
+  open(){
+    this.setState({ expanded: !this.state.expanded })
+  }
+  close(){
+    this.setState({ expanded: !this.state.expanded })
+  }
   render() {
+    const info = this.props.CharInfo;
+    if(!this.state.expanded){
+      return <p></p>
+    }
     return(
       <div></div>
 
